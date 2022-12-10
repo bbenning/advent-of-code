e9cd0a0aa52fd877f6b1f63e5a66f23605c21d35
Bas Benning <bas.benning@gmail.com>
2022-12-10 10:51:10 +0100

    puzzle 10

---
+++ src/main/kotlin/nl/bbenning/advent/_2022/_10/Main.kt
@@ -0,0 +1,41 @@
+package nl.bbenning.advent._2022._10
+
+import java.io.File
+import java.math.BigInteger
+
+object Main {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val inputStrings = File("./src/main/resources/inputs/2022/input10.txt").readLines()
+
+
+        val output = inputStrings.fold(listOf(1)) { acc, s ->
+            val curVal = acc.last()
+            if (s == "noop") {
+                acc + curVal
+            } else {
+                acc + curVal + (curVal + s.split(" ")[1].toInt())
+            }
+        }
+
+        val solution10a = output.mapIndexed { index, i ->
+            val cycle = index + 1
+            if (cycle == 20 || (cycle - 20) % 40 == 0) i * cycle else 0
+        }.fold(0) { acc, i -> acc + i }
+
+        println("Answer to 10a: $solution10a")
+
+        val printedLines = output.chunked(40).map { shiftValues ->
+            shiftValues.foldIndexed("") { index, acc, i ->
+                val takePixelAt = index - i + 1
+                acc + if (takePixelAt in 0 .. 3) "#" else "."
+            }
+        }
+
+        printedLines.forEach { println(it) }
+
+        // 13480
+        // EGJBGCFK
+    }
+}
