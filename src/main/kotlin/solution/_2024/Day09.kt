c914580cfd2c1b5b5d9af6f89f95f2cca724cde6
bbenning <bas.benning@gmail.com>
2024-12-09 20:47:34 +0100

    2024 day 9 cleaned it up a bit

--- src/main/kotlin/solution/_2024/Day09.kt
+++ src/main/kotlin/solution/_2024/Day09.kt
@@ -1,116 +1,73 @@
 package solution._2024
 
 class Day09(val input: String) {
+    private data class Block(val id: Long, val used: Int, val free: Int) {
+        fun checkSum(startIdx: Int): Long = (startIdx until startIdx + used).sumOf { it * id }
+
+    }
+
+    private val initialDiskMap = input.map { it.digitToInt() }.let{if(it.size % 2 == 0) it else it + 0}.chunked(2)
+        .mapIndexed{ idx, (a, b) -> Block(idx.toLong(), a, b)}
+
     fun solve1(): Long {
-        return solve()
+        val disk = initialDiskMap.toMutableList()
+
+        var idx = 0
+        while(idx < disk.size - 1) {
+            val block = disk[idx]
+            if(block.free == 0) {
+                idx++
+            } else {
+                val lastBlock = disk.removeLast()
+                val moveSize = minOf(lastBlock.used, block.free)
+                disk.add(idx + 1, Block(lastBlock.id, moveSize, block.free - moveSize))
+                disk[idx] = block.copy(free = 0)
+                if(lastBlock.used > moveSize) {
+                    disk.add(lastBlock.copy(used = lastBlock.used - moveSize))
+                }
+            }
+        }
+
+        val checksum = disk.fold(Pair(0L, 0)) { acc, block ->
+            Pair(acc.first + block.checkSum(acc.second), acc.second + block.used + block.free)
+        }.first
+        return checksum
     }
 
     fun solve2(): Long {
-        abstract class DiskSpace
-        data class File(val id: Int, val size: Int) : DiskSpace()
-        data class FreeSpace(val size: Int): DiskSpace()
+        val disk = initialDiskMap.toMutableList()
 
-        val diskMap = input.map { it.digitToInt() }.foldIndexed(emptyList<DiskSpace>()) { idx, acc, d ->
-            if(idx % 2 == 0) {
-                acc + File(idx / 2, d)
-            } else {
-                acc + FreeSpace(d)
+        var idx = disk.size - 1
+        var filesRemaining = disk.size
+        while(idx > 0) {
+
+            val curBlock = disk[idx]
+            if(curBlock.id > filesRemaining) {
+                // already moved, so let's move on
+                idx--
+                continue
             }
-        }.toMutableList()
 
-        val numFiles = diskMap.count { it is File }
-        var filesRemaining = numFiles
-        var idx  = diskMap.size - 1
-        while(filesRemaining > 0) {
-            val workingOn = diskMap[idx]
-            when(workingOn) {
-                is File -> {
-                    if(workingOn.id >= filesRemaining) {
-                        // leave be, already moved
-                        idx--
-                    } else {
-                        // find first free space
-                        val firstFreeIdx = diskMap.indexOfFirst { it is FreeSpace && it.size >= workingOn.size }
-                        if(firstFreeIdx < 0 || firstFreeIdx >= idx) {
-                            // cannot move, so move on
-                            idx--
-                        } else {
-                            val freeSpace = diskMap[firstFreeIdx] as FreeSpace
-
-                            diskMap[firstFreeIdx] = workingOn
-                            diskMap[idx] = FreeSpace(workingOn.size)
-                            val leftOverSpace = freeSpace.size - workingOn.size
-                            if(leftOverSpace > 0) {
-                                diskMap.add(firstFreeIdx + 1, FreeSpace(leftOverSpace))
-                            } else {
-                                idx--
-                            }
-                        }
-                        filesRemaining--
-                    }
-                }
-                is FreeSpace -> {
-                    idx --
-                }
+            val firstAvailableBlockIdx = disk.indexOfFirst { it.free >= curBlock.used }
+            if(firstAvailableBlockIdx < 0 || firstAvailableBlockIdx >= idx) {
+                // no suitable free space found, or further away, so let's move on
+                idx--
+                filesRemaining--
+                continue
             }
+
+            val firstAvailableBlock = disk[firstAvailableBlockIdx]
+            disk[firstAvailableBlockIdx] = firstAvailableBlock.copy(free = 0)
+            disk[idx] = Block(-1, 0, curBlock.used + curBlock.free)
+            disk.add(firstAvailableBlockIdx + 1, curBlock.copy(free = firstAvailableBlock.free - curBlock.used))
 
         }
 
-        val checkSum = diskMap.foldIndexed(Pair(0L, 0)){idx, acc, diskSpace ->
-            when(diskSpace) {
-                is File -> {
-                    Pair(acc.first + (0 until diskSpace.size).sumOf{ diskSpace.id.toLong() * (it + acc.second) }, acc.second + diskSpace.size)
-                }
-                is FreeSpace -> Pair(acc.first, acc.second + diskSpace.size)
-                else -> throw IllegalStateException("Cannot happen")
-            }
+        val checksum = disk.fold(Pair(0L, 0)) { acc, block ->
+            Pair(acc.first + block.checkSum(acc.second), acc.second + block.used + block.free)
         }.first
-        return checkSum
-    }
-
-    private fun solve(): Long {
-        val (files, freeSpaces) = input.map { it.digitToInt() }.foldIndexed(Pair(emptyList<Int>(), emptyList<Int>())) { idx, acc, d ->
-            if(idx % 2 == 0) {
-                Pair(acc.first + d, acc.second)
-            } else {
-                Pair(acc.first, acc.second + d)
-            }
-        }.let { Pair(it.first.toMutableList(), it.second.toMutableList()) }
-
-        var fileId = 0
-        var freeSpaceId = 0
-        var position = 0L
-
-        val listOfFileIds = mutableListOf<Int>()
-
-        while(fileId < files.size) {
-            if(fileId == freeSpaceId) {
-                val fileLength = files[fileId]
-                repeat(fileLength) {
-                    listOfFileIds.add(fileId)
-                    position++
-                }
-                fileId++
-            } else {
-                val freeSpaceLength = freeSpaces[freeSpaceId]
-                repeat(freeSpaceLength) {
-                    val lastFileId = files.size - 1
-                    if(lastFileId>fileId) {
-                        listOfFileIds.add(lastFileId)
-                        position++
-
-                        files[lastFileId]--
-                        if(files[lastFileId] == 0) {
-                            files.removeLast()
-                        }
-                    }
-                }
-                freeSpaceId++
-            }
-        }
-
-        val checksum = listOfFileIds.foldIndexed(0L){idx, acc, fileId -> acc + idx * fileId}
         return checksum
     }
 }
+
 
